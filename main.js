0bc21ab0820344daaea684cfe768d02813e2bcc1
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2022-08-13 11:39:16 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+
+serve({
+  "/": () => new Response("hello world"),
+  "/blog/:slug": (request, connInfo, params) => {
+    const post = `Hello, you visited ${params.slug}!`;
+    return new Response(post);
+  },
+  // The route handler of 404 will be invoked when a route handler
+  // for the requested path is not found.
+  404: () => new Response("not found"),
+});
