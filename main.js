d6f2827ec0b70b76f333168f47116b1db82fe7d5
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2022-08-13 12:16:25 +0700

    d

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import {
+   serve,
+   json
+ } from "https://deno.land/x/sift@0.5.0/mod.ts";
+ const quotes = [
+  {
+    quote: "Those who can imagine anything, can create the impossible.",
+    author: "Alan Turing",
+  },
+  {
+    quote: "Any sufficiently advanced technology is equivalent to magic.",
+    author: "Arthur C. Clarke",
+  },
+];
 
 async function handledata(){
-  return new Response("tolol")
+  return json({quotes})
 
 }
 serve({
-  "/":()=> handledata,
+  "/":handledata ,
 
 });
 
