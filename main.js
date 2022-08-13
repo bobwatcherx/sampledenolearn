8327182becd85fddd6716b6637522b2b0118ff04
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2022-08-13 12:12:46 +0700

    2

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 
+async function handledata(){
+  return new Response("tolol")
+
+}
 serve({
-  "/": handledata,
+  "/": handledata(),
 
 });
 
-const handledata = async()=>{
-  new Response("tolol")
-}
