e265c06112943d92f28eb30b1c77820297d721f4
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2022-08-13 12:39:11 +0700

    121

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ import {
  } from "https://deno.land/x/sift@0.5.0/mod.ts";
  const quotes = [
   {
-    quote: "Those who can imagine anything, can create the impossible.",
-    author: "Alan Turing",
+    CanvasCaptureMediaStreamTrack: "Those who can imagine anything, can create the impossible.",
+    c: "cccc Turing",
   },
-  {
-    quote: "Any sufficiently advanced technology is equivalent to magic.",
-    author: "Arthur C. Clarke",
-  },
+
 ];
 
 async function handledata(request:Request){
